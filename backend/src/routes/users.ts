2cdd73b92751cb742aeb96ab98ad8ae15c8378b9
sandrafmoreira <sandramoreira.im@gmail.com>
2025-11-25 12:14:15 +0000

    deleted duplicated files and folders

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,11 +1,12 @@
 import {Router} from "express";
 import * as usersControllers from "../controllers/users";
+import {auth} from "../middleware/auth"
 
 const router = Router();
 
 router.post("/register", usersControllers.registerUser);
 router.post("/login", usersControllers.loginUser);
 
-router.get("/", usersControllers.getAllUsers);
+router.get("/", auth, usersControllers.getAllUsers);
 
 export default router;
