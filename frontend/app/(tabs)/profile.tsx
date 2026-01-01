6a6416a0c268a43934ec172bc5e05a3efd6d6638
kenLukau31 <kenlukau@gmail.com>
2026-01-01 23:23:49 +0000

    costumer priv-profile

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -5,7 +5,8 @@ import {
   TouchableOpacity,
   StyleSheet,
   FlatList,
-  useColorScheme, 
+  useColorScheme,
+  Image,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -13,10 +14,10 @@ import { Ionicons } from "@expo/vector-icons";
 import { ProfileHeader } from "@/components/profile-components/ProfileHeader";
 import { MarketCard } from "@/components/profile-components/MarketCard";
 import { SellerCard } from "@/components/profile-components/SellerCard";
-import { PrimaryButton } from '@/components/ui/PrimaryButton';
+import { PrimaryButton } from "@/components/ui/PrimaryButton";
 
 // Imports thematization
-import { Colors } from "@/constants/theme"; 
+import { Colors } from "@/constants/theme";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 
@@ -24,39 +25,46 @@ import { MARKETS, SELLERS } from "@/constants/data";
 
 const currentUser = SELLERS[0];
 
-export default function SellerProfile() {
-
-  const colorScheme = useColorScheme() ?? 'light';
+export default function ProfileScreen() {
+  const isSeller = false;
+  const colorScheme = useColorScheme() ?? "light";
   const currentColors = Colors[colorScheme];
 
   return (
-    <ThemedView style={styles.container}> 
-      
+    <ThemedView style={styles.container}>
       <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
-        
         {/* Header Section */}
-        <View style={[styles.headerSection, { backgroundColor: currentColors.background }]}>
-          
+        <View
+          style={[
+            styles.headerSection,
+            { backgroundColor: currentColors.background },
+          ]}
+        >
           {/* Icon Settings */}
           <TouchableOpacity
             style={styles.topRightIcon}
             onPress={() => console.log("Settings")}
           >
-            <Ionicons name="settings-sharp" size={26} color={currentColors.tint} />
+            <Ionicons
+              name="settings-sharp"
+              size={26}
+              color={currentColors.tint}
+            />
           </TouchableOpacity>
 
           {/* Profile Component */}
           <ProfileHeader user={currentUser} />
 
-          {/* Button */}
-
-          <View style={{ marginTop: 10 }}>
-            <PrimaryButton 
-              title="Criar Anúncio"
-              iconName="add-sharp"
-              onPress={() => console.log("Criar")} 
-            />
-          </View>
+          {/* Button for sellers */}
+          {isSeller && (
+            <View style={{ marginTop: 10 }}>
+              <PrimaryButton
+                title="Criar Anúncio"
+                iconName="add-sharp"
+                onPress={() => console.log("Criar")}
+              />
+            </View>
+          )}
         </View>
 
         {/* Markets */}
@@ -64,7 +72,7 @@ export default function SellerProfile() {
           <ThemedText type="subtitle" style={styles.sectionTitle}>
             Feiras Favoritas
           </ThemedText>
-          
+
           <FlatList
             horizontal
             data={MARKETS}
@@ -85,7 +93,7 @@ export default function SellerProfile() {
           <ThemedText type="subtitle" style={styles.sectionTitle}>
             Feirantes Favoritos
           </ThemedText>
-          
+
           <FlatList
             horizontal
             data={SELLERS}
@@ -100,39 +108,95 @@ export default function SellerProfile() {
             )}
           />
         </View>
+
+        {/* became a seller */}
+        {!isSeller && (
+          <View style={styles.becomeSellerContainer}>
+            <View style={styles.divider} />
+
+            <ThemedText type="subtitle" style={{ marginBottom: 10 }}>
+              Queres tornar-te feirante?
+            </ThemedText>
+
+            <ThemedText style={{ marginBottom: 20 }}>
+              Divulga os teus produtos e associa-te às feiras em que participas.
+            </ThemedText>
+
+          <Image      
+              source={require("../../assets/markets/vegetables.png")} 
+              style={{ width: "100%", height: 120, marginBottom: 20 }}
+              resizeMode="contain"
+            />
+
+            <PrimaryButton
+              title="Criar Perfil Público"
+              onPress={() => console.log("Criar perfil")}
+              style={{ width: "100%", marginBottom: 10 }}
+            />
+
+            {/* <PrimaryButton 
+                    title="Ignorar" 
+                    onPress={() => console.log("Ignorar")}
+                    style={{ 
+                        width: '100%', 
+                        backgroundColor: currentColors.background, 
+                        borderColor: '#E0E0E0' 
+                    }}
+                    textColor={Colors[colorScheme].text}
+                /> */}
+          </View>
+        )}
       </ScrollView>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
+  container: {
+    flex: 1,
   },
 
   headerSection: {
-    paddingTop: 60, 
+    paddingTop: 60,
     paddingBottom: 20,
     paddingHorizontal: 20,
-    alignItems: "center", 
+    alignItems: "center",
     position: "relative",
-    
   },
 
   topRightIcon: {
     position: "absolute",
-    top: 50, 
+    top: 50,
     right: 20,
     zIndex: 10,
   },
 
-  section: { 
-    paddingLeft: 20, 
-    marginTop: 30 
+  section: {
+    paddingLeft: 20,
+    marginTop: 30,
   },
-  
-  sectionTitle: { 
+
+  sectionTitle: {
     marginBottom: 15,
-    
+  },
+
+  becomeSellerContainer: {
+    padding: 20,
+    marginTop: 20,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: "#E0E0E0",
+    marginBottom: 30,
+    width: "100%",
+  },
+  placeholderImage: {
+    height: 100,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 10,
   },
 });
